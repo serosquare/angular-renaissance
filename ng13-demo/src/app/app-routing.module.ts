99be8db324a1ad481659af761e1163e765a58b5e
Sebastian Rodriguez <sebastian.rodriguez@unosquare.com>
2024-01-14 19:17:03 -0500

    feat(ng13-demo): Home Page

--- ng13-demo/src/app/app-routing.module.ts
+++ ng13-demo/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule) }];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
