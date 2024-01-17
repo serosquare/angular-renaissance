16f74854d00801e50a0d5b8a19255a7456c9c676
Sebastian Rodriguez <sebastian.rodriguez@unosquare.com>
2024-01-16 20:47:10 -0500

    feat(ng15-demo): Setup project basics

---
+++ ng15-demo/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  template: `
+    <header id="header" class="header">
+      <nav id="nav" class="nav">
+        <img class="nav_logo" src="/assets/unosquare/unosquare-logo.png" alt="Unosquare Logo">
+        <ul class="nav_elements">
+          <li class="nav_elements_item">
+            <a class="nav_elements_item-link" routerLink="/">Home</a>
+          </li>
+          <li class="nav_elements_item">
+            <a class="nav_elements_item-link" routerLink="/posts">Posts</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng15-demo';
+}
---
+++ ng15-demo/src/main.ts
@@ -0,0 +1,19 @@
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// import { AppModule } from './app/app.module';
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { provideRouter } from "@angular/router";
+
+bootstrapApplication(
+  AppComponent,
+  {
+    providers: [
+      provideRouter([])
+    ],
+  }
+)
